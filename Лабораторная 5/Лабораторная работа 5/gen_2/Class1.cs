837d105210a068029cbe18daece1806c31b2c934
19Semen99 <58047892+19Semen99@users.noreply.github.com>
2023-01-24 13:32:22 +0300

    Add files via upload

---
+++ Лабораторная 5/Лабораторная работа 5/gen_2/Class1.cs
@@ -0,0 +1,169 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gen_2
+{
+    class Chromosome
+    {
+        public double xVal;
+        public double yVal;
+        public double funcVal;
+
+        private ScatterSeries point;
+        public Chromosome()
+        {
+        }
+        public Chromosome(double xval, double yval, double fun)
+        {
+
+            xVal = xval;
+            yVal = yval;
+            funcVal = fun;
+        }
+        public Chromosome(Chromosome c)
+        {
+
+            xVal = c.xVal;
+            yVal = c.yVal;
+            funcVal = c.funcVal;
+        }
+        public ScatterSeries Point
+        {
+            get
+            {
+                this.point = new ScatterSeries();
+                this.point.ColorAxisKey = "ColorAxis";
+                this.point.MarkerSize = 8;
+                this.point.MarkerType = MarkerType.Plus;
+
+                point.Points.Add(new ScatterPoint(xVal, yVal, double.NaN, 1));
+                return point;
+            }
+            set => point = value;
+        }
+        public void ReCreateFull(double xval, double yval, double fun)
+        {
+            xVal = xval;
+            yVal = yval;
+            funcVal = fun;
+        }
+    }
+
+
+
+    class FitnessFunc
+    {
+
+        public static readonly double minInt = -5.12;
+        public static readonly double maxInt = 5.12;
+        public static double Func(double x, double y)
+        {
+            return (x * x) + (2 * y * y);
+        }
+    }
+
+    class GeneticAlgorithm
+    {
+
+        private Random random = new Random();
+        private StringBuilder textAll = new StringBuilder();
+        List<Chromosome> genome;
+
+        public string TextAll { get { return textAll.ToString(); } }
+        public GeneticAlgorithm() { }
+        public Chromosome Evolution(int iterationsCount, double xMut, double yMut)
+        {
+
+            textAll.Clear();
+            double minInterval = FitnessFunc.minInt;
+            double maxInterval = FitnessFunc.maxInt;
+            int currentCount = 0;
+
+
+
+            textAll.Append("Исходная популяция" + Environment.NewLine);
+            printGenome(genome);
+            Chromosome best = genome.Find(g => g.funcVal == genome.Min(v => v.funcVal));
+            for (int i = 0; i < iterationsCount; i++)
+            {
+                foreach (Chromosome chr in genome)
+                {
+                    Chromosome c = new Chromosome();
+                    if (random.Next(0, 2) == 0)
+                        c.xVal = chr.xVal + random.NextDouble() * xMut;
+                    else
+                        c.xVal = chr.xVal - random.NextDouble() * xMut;
+                    if (random.Next(0, 2) == 0)
+                        c.yVal = chr.yVal + random.NextDouble() * yMut;
+                    else
+                        c.yVal = chr.yVal - random.NextDouble() * yMut;
+                    c.funcVal = FitnessFunc.Func(c.xVal, c.yVal);
+                    if (c.funcVal < chr.funcVal)
+                    {
+                        chr.xVal = c.xVal;
+                        chr.yVal = c.yVal;
+                        chr.funcVal = c.funcVal;
+                    }
+                }
+                //textAll.Append("сокращённая популяция" + Environment.NewLine);
+                //printGenome(genome);
+                textAll.Append("Среднее значение: " + genome.Average(g => g.funcVal) + Environment.NewLine + "Минимальное значение: " + genome.Min(g => g.funcVal) + Environment.NewLine);
+                currentCount++;
+                best = genome.Find(g => g.funcVal == genome.Min(v => v.funcVal));
+            }
+
+            textAll.Append("Финальный геном" + Environment.NewLine);
+            printGenome(genome);
+
+            textAll.Append("Итераций: " + currentCount + " Лучшая: " + "X: " + best.xVal + " | " + "Y: " + best.yVal + " | F: " + best.funcVal + Environment.NewLine);
+
+            return best;
+
+        }
+
+        //private void Reproduction()
+        //{
+        //    Chromosome cr;
+        //    for (int i = 0; i < genome.Count; i++)
+        //    {
+        //        for (int j = 0; j < genome.Count; j++)
+        //        {
+        //            if (genome[i].funcVal < genome[j].funcVal)
+        //            {
+        //                cr = genome[i];
+        //                genome[i] = genome[j];
+        //                genome[j] = cr;
+        //            }
+        //        }
+        //    }
+        //}
+
+        public void CreateGenome(int count, double xMin, double xMax, double yMin, double yMax)
+        {
+            genome = new List<Chromosome>();
+            for (int i = 0; i < count; i++)
+            {
+                genome.Add(new Chromosome((random.NextDouble() * (xMax - xMin) + xMin), (random.NextDouble() * (yMax - yMin) + yMin), 0));
+                genome[i].funcVal = FitnessFunc.Func(genome[i].xVal, genome[i].yVal);
+            }
+
+        }
+
+        public void printGenome(List<Chromosome> genome)
+        {
+
+            int i = 0;
+            foreach (Chromosome c in genome)
+            {
+                textAll.Append("[" + i++ + "] " + "x = " + c.xVal + " y = " + c.yVal + " F = " + c.funcVal + Environment.NewLine);
+            }
+
+        }
+
+    }
+}
---
+++ Лабораторная 5/Лабораторная работа 5/gen_2/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using Newtonsoft.Json;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gen_2
+{
+    public partial class MainWindow : Window
+    {
+
+        private PlotModel model;
+
+        private int sizeMap = 200;
+        public double startPoint = -5.12, endPoint = 5.12;
+
+        private ScatterSeries point = new ScatterSeries();
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            initHeatMap();
+            point.ColorAxisKey = "ColorAxis";
+            point.MarkerSize = 8;
+            point.MarkerType = MarkerType.Plus;
+
+        }
+        
+        private void initHeatMap()
+        {
+            model = new PlotModel { Title = "Heatmap" };
+            model.Axes.Add(new LinearColorAxis { Palette = OxyPalettes.Viridis(255) });
+
+            var data = new double[sizeMap, sizeMap];
+            double xValue = startPoint, yValue = startPoint, max = endPoint;
+            double det = (Math.Abs(xValue) + max) / sizeMap;
+
+            for (int y = 0; y < sizeMap; ++y)
+            {
+                xValue = startPoint;
+                for (int x = 0; x < sizeMap; ++x)
+                {
+                    data[x, y] = getFunction(xValue, yValue);
+                    xValue += det;
+                }
+                yValue += det;
+            }
+
+            var heatMapSeries = new HeatMapSeries
+            {
+                X0 = startPoint,
+                X1 = endPoint,
+                Y0 = startPoint,
+                Y1 = endPoint,
+                Interpolate = true,
+                RenderMethod = HeatMapRenderMethod.Bitmap,
+                Data = data
+            };
+
+            var axis1 = new LinearColorAxis();
+            axis1.Key = "ColorAxis";
+            axis1.IsAxisVisible = true;
+            model.Axes.Add(axis1);
+
+            axis1.Palette.Colors.Clear();
+            axis1.Palette.Colors.Add(OxyColor.FromArgb((byte)255, 255, 0, 0));
+
+            model.Series.Add(heatMapSeries);
+            testPlotView.Model = model;
+        }
+
+
+        public double getFunction(double x, double y)
+        {
+            return (x * x) + (2 * y * y);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int kolChromosome = Convert.ToInt32(textBlock2.Text);
+            geneticAlgorithm.CreateGenome(kolChromosome, startPoint, endPoint, startPoint, endPoint);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+            int kolIteration = Convert.ToInt32(textBlock1.Text);
+
+            double xMut = Convert.ToDouble(textBlock3.Text);
+            double yMut = Convert.ToDouble(textBlock4.Text);
+
+            Chromosome components = geneticAlgorithm.Evolution(kolIteration, xMut, yMut);
+            double x = components.xVal;
+            double y = components.yVal;
+            initHeatMap();
+            var random = new Random();
+            textBox1.Clear();
+            textBox1.AppendText(geneticAlgorithm.TextAll + Environment.NewLine);
+            while (model.Series.Count > 1)
+            {
+                model.Series.Remove(model.Series.Last());
+            }
+            model.Series.Add(components.Point);
+            testPlotView.Model.InvalidatePlot(true);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+
+    }
+
+}
